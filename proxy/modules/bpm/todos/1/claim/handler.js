5b3930e8d6b03df8e416b938fa5579d0e6e86b24
小镜子 <897204198@qq.com>
2018-11-06 09:51:02 +0800

    Revert "add 配置"
    This reverts commit 72adf7cff4ca3c6f95a8611827da687a87d3d7c3.

--- proxy/modules/bpm/todos/1/claim/handler.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var handler = module.exports = {};
-var common = require('../../../common.js');
-
-handler.onPost = function (req, res, data) {
-  res.writeHeader(200, {'Content-Type': 'application/json'});
-  res.end('');
-};
--- proxy/modules/notices/handler.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var handler = module.exports = {};
-var common = require('../common.js');
-
-handler.onGet = function (req, res, data) {
-  common.jsonRes(req, res, '/all');
-};
